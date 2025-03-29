3bf8f77023680cdc653d28e740c81d75da2b4ba0
Daliadot <gh8802871@gmail.com>
2025-03-28 22:41:26 -0300

    CRUD-complit

--- app/Http/Controllers/CriptomoedaController.php
+++ app/Http/Controllers/CriptomoedaController.php
@@ -1,84 +1,150 @@
 <?php
-
+ 
 namespace App\Http\Controllers;
-
+ 
 use App\Models\criptomoeda;
 use Illuminate\Http\Request;
-
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Validator;
-
-
-class CriptomoedaController extends Controller
+ 
+class criptomoedaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        $regBook = Criptomoedas::All();
+        $regBook = criptomoeda::All();
         $contador = $regBook->count();
-
-        return Response()->json($regBook);
+ 
+        if ($contador > 0) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Criptos encontradas com sucesso',
+                'data' => $regBook,
+                'total' => $contador
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Nenhuma cripto encontrada',
+            ], 404);
+        }
     }
-
-    /**
-     * Store a newly created resource in storage.
-     */
+ 
     public function store(Request $request)
     {
-        $validator = Validator::make($request->all(),[
+        $validator = Validator::make($request->all(), [
             'sigla' => 'required',
             'nome' => 'required',
-            'valor' => 'required'
+            'valor' => 'required',
         ]);
-
-        if ($validator->fails()){
+ 
+        if ($validator->fails()) {
             return response()->json([
                 'success' => false,
                 'message' => 'Registros inválidos',
                 'errors' => $validator->errors()
-            ],400);
+            ], 400);
         }
-
-        $registros = criptomoeda::create($request->all());
-        
-        if($registros) {
+ 
+        $regBook = criptomoeda::create($request->all());
+ 
+        if ($regBook) {
             return response()->json([
                 'success' => true,
-                'message' => 'Criptomoeda cadastrada com sucesso!',
-                'data' => $registros
-            ],201);
-        }else {
+                'message' => 'Cripto cadastrada com sucesso!',
+                'data' => $regBook
+            ], 201);
+        } else {
             return response()->json([
                 'success' => false,
-                'message' => 'Erro ao cadstrar a criptomoeda'
-            ],500);
+                'message' => 'Erro ao cadastrar a cripto moeda'
+            ], 500);
         }
     }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(criptomoeda $criptomoeda)
+ 
+    public function show($id)
     {
-        //
+        $regBook = criptomoeda::find($id);
+ 
+        if ($regBook) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Cripto localizada com sucesso',
+                'data' => $regBook
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Cripto não localizada'
+            ], 404);
+        }
     }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, criptomoeda $criptomoeda)
+ 
+    public function update(Request $request, string $id)
     {
-        //
+        $regBook = criptomoeda::find($id);
+       
+        if (!$regBook) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Cripto não encontrada'
+            ], 404);
+        }
+ 
+        $validator = Validator::make($request->all(), [
+            'sigla' => 'required',
+            'nome' => 'required',
+            'valor' => 'required',
+        ]);
+ 
+        if ($validator->fails()) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Registros inválidos',
+                'errors' => $validator->errors()
+            ], 400);
+        }
+ 
+        $regBook->sigla = $request->sigla;
+        $regBook->nome = $request->nome;
+        $regBook->valor = $request->valor;
+ 
+        if ($regBook->save()) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Cripto atualizada com sucesso',
+                'data' => $regBook
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro ao atualizar cripto'
+            ], 500);
+        }
     }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(criptomoeda $criptomoeda)
+ 
+    public function destroy($id)
     {
-        //
+        $regBook = criptomoeda::find($id);
+ 
+        if (!$regBook) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Cripto não encontrada'
+            ], 404);
+        }
+ 
+        if ($regBook->delete()) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Cripto deletada com sucesso'
+            ], 200);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Erro ao deletar cripto'
+            ], 500);
+        }
     }
 }
+ 
--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,12 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CriptomoedaController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/', function(){return response()->json(['Sucesso'=>true]);}); 
+Route::get('/cripto',[CriptomoedaController::class,'index']);
+Route::get('/cripto/{id}', [CriptomoedaController::class,'show']);
 
 Route::post('/cripto',[CriptomoedaController::class,'store']);
+
+Route::put('/cripto/{id}',[CriptomoedaController::class,'update']);
+
+Route::delete('/cripto/{id}',[CriptomoedaController::class,'destroy']);
